29246bb51edfdb8dc1960f62f6ed53a41560bd49
kevinbdsouza <kdsouza1496@gmail.com>
2020-05-13 20:28:21 -0700

    updsate

--- src/testing_scripts/test_downstream.py
+++ src/testing_scripts/test_downstream.py
@@ -144,7 +144,7 @@ r2_avo = r2_avo / np.mean(r2_avo)
 path = "/home/kevindsouza/Documents/projects/latentGenome/src/common/data/"
 r2_lstm = np.load(path + "mod_euclid_lstm.npy")
 r2_avo = np.load(path + "mod_euclid_avocado.npy")
-pos = [0, 100, 200, 400, 800, 1600, 3200, 6400, 9000, 12800] 
+pos = [0, 100, 200, 400, 800, 1600, 3200, 6400, 9000, 10000]
 pos = [i * 10000 for i in pos]
 
 plt.ylabel('R-squared', fontsize=14)
